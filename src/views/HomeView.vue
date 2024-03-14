994f9d2f77e4949f4a4c8311e58da6fd940fbfb3
brandonwilbert8 <111483690+brandonwilbert8@users.noreply.github.com>
2024-03-14 14:33:16 +1100

    Added animation transition for the main content and name page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="flex md:flex-row flex-col md:items-center p-6 gap-2 sm:gap-8 mt-40 md:mt-52">
+  <div class="flex md:flex-row flex-col md:items-center p-6 gap-2 sm:gap-8 mt-36 md:mt-44">
     <img src="../assets/images/brandon_avatar.jpeg" class="w-40 h-40 md:w-44 md:h-44 rounded-full" />
     <div class="flex flex-col md:ml-10">
       <p class="text-4xl md:text-6xl text-blue100 font-light mt-4">Hi! I'm <strong class="font-sans">Brandon</strong></p>
