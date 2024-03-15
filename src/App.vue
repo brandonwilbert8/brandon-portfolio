31697b4b4ad29bdbc8875cb1ec8e7e19e6456530
brandonwilbert8 <111483690+brandonwilbert8@users.noreply.github.com>
2024-03-15 16:37:06 +1100

    Added toggle emoji button

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ export default {
     return {
       isOpen: false,
       activeTab: 'home',
+      isDarkMode: false,
     };
   },
   created() {
@@ -30,7 +31,68 @@ export default {
 <template>
   <div class="min-h-screen flex justify-center items-center">
     <div class="container mx-auto px-5 md:max-w-screen-md w-full h-screen">
-      <header class="sm:flex sm:items-center sm:px-4 sm:py-3 mt-10 md:mt-14">
+      <div class="flex justify-between">
+        <header class="sm:flex sm:items-center sm:px-4 sm:py-3 mt-10 md:mt-14">
+          <div class="flex items-center justify-between px-4 py-3 sm:p-0">
+            <div class="sm:hidden">
+              <button @click="isOpen = !isOpen" type="button" class="block text-blue100 hover:text-indigo-500 focus:text-blue100 focus:outline-none">
+                <svg class="h-8 w-8 fill-current" viewBox="0 0 24 24">
+                  <path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z" />
+                  <path v-if="!isOpen" fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z" />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <nav :class="isOpen ? 'block' : 'hidden'" class="px-2 pt-2 pb-4 sm:flex sm:p-0 gap-5 ml-1">
+            <router-link
+              to="/home"
+              :class="{ 'text-blue100': activeTab === 'home', 'text-gray-800': activeTab !== 'home', 'font-light': activeTab !== 'home' }"
+              class="block px-2 py-1 font-semibold rounded hover:bg-blue-100 text-xl"
+              @click="
+                {
+                  activeTab = 'home';
+                  isOpen = false;
+                }
+              "
+              >Home</router-link
+            >
+            <router-link
+              to="/about"
+              :class="{ 'text-blue100': activeTab === 'about', 'text-gray-800': activeTab !== 'about', 'font-light': activeTab !== 'about' }"
+              class="block px-2 py-1 font-semibold rounded hover:bg-blue-100 text-xl"
+              @click="
+                activeTab = 'about';
+                isOpen = false;
+              "
+              >About</router-link
+            >
+            <router-link
+              to="/project"
+              :class="{ 'text-blue100': activeTab === 'project', 'text-gray-800': activeTab !== 'project', 'font-light': activeTab !== 'project' }"
+              class="mt-1 block px-2 py-1 font-semibold rounded hover:bg-blue-100 sm:mt-0 sm:ml-2 text-xl"
+              @click="
+                activeTab = 'project';
+                isOpen = false;
+              "
+              >Projects</router-link
+            >
+            <router-link
+              to="/contact"
+              :class="{ 'text-blue100': activeTab === 'contact', 'text-gray-800': activeTab !== 'contact', 'font-light': activeTab !== 'contact' }"
+              class="mt-1 block px-2 py-1 font-semibold rounded hover:bg-blue-100 sm:mt-0 sm:ml-2 text-xl"
+              @click="
+                activeTab = 'contact';
+                isOpen = false;
+              "
+              >Contact</router-link
+            >
+          </nav>
+        </header>
+        <div class="mt-12 md:mt-16">
+          <button class="text-5xl">{{ isDarkMode ? '🌚' : '🌞' }}</button>
+        </div>
+      </div>
+      <!-- <header class="sm:flex sm:items-center sm:px-4 sm:py-3 mt-10 md:mt-14">
         <div class="flex items-center justify-between px-4 py-3 sm:p-0">
           <div class="sm:hidden">
             <button @click="isOpen = !isOpen" type="button" class="block text-blue100 hover:text-indigo-500 focus:text-blue100 focus:outline-none">
@@ -85,7 +147,7 @@ export default {
             >Contact</router-link
           >
         </nav>
-      </header>
+      </header> -->
       <div class="p-6 text-left">
         <transition name="slide-fade">
           <div v-if="activeTab !== 'home'">
